296f86f9d5de8bc0221199446637db5c92d0a303
Josh <j0522469@amarillocollege.com>
2021-03-24 15:25:27 -0500

    Add project files.

---
+++ WebScrapingTool/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace WebScrapingTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.scrapBTN = new System.Windows.Forms.Button();
+            this.txtdata = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // scrapBTN
+            // 
+            this.scrapBTN.Location = new System.Drawing.Point(12, 44);
+            this.scrapBTN.Name = "scrapBTN";
+            this.scrapBTN.Size = new System.Drawing.Size(133, 38);
+            this.scrapBTN.TabIndex = 0;
+            this.scrapBTN.Text = "Scrap Now";
+            this.scrapBTN.UseVisualStyleBackColor = true;
+            this.scrapBTN.Click += new System.EventHandler(this.scrapBTN_Click);
+            // 
+            // txtdata
+            // 
+            this.txtdata.Location = new System.Drawing.Point(217, 12);
+            this.txtdata.Name = "txtdata";
+            this.txtdata.Size = new System.Drawing.Size(548, 414);
+            this.txtdata.TabIndex = 1;
+            this.txtdata.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtdata);
+            this.Controls.Add(this.scrapBTN);
+            this.Name = "Form1";
+            this.Text = "WebScrapper";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button scrapBTN;
+        private System.Windows.Forms.RichTextBox txtdata;
+    }
+}
+
---
+++ WebScrapingTool/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebScrapingTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void scrapBTN_Click(object sender, EventArgs e)
+        {
+            HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = web.Load("https://www.yellowpages.com/search?search_terms=restaurant&geo_location_terms=Amarillo%2C+TX");
+
+            var HeaderNames = doc.DocumentNode.SelectNodes("//a[@class= 'business-name']").ToList();
+
+            string ListData = "";
+            foreach (var item in HeaderNames)
+            {
+
+                ListData += Environment.NewLine + item.InnerText;
+            
+            }
+            txtdata.Text = ListData;
+
+        }
+    }
+}
